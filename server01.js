a259b5942b2289d23576fd49ac510514a84ab549
kodziarz <gregor47@poczta.fm>
2022-01-07 15:15:45 +0100

    dodanie wysłania obiektu na get-cie /data

--- server01.js
+++ server01.js
@@ -6,6 +6,16 @@ app.get("/", function (req, res) {
     res.send("<h1>my app on heroku!</h1>")
 })
 
+app.get("/data", function (req, res){
+    let object = {
+        imie: "Grzegorz",
+        nazwisko: "Lenarski",
+        klasa: "3I2",
+        grupa: "2"
+    }
+    res.send(JSON.stringify(object, null, 5))
+})
+
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
